fce4dbc83ac7eb7af6940332f4e97c970539d80c
maa-atk <maa.abhitk@gmail.com>
2020-04-17 12:03:48 +0530

    Added Academic History UI

---
+++ lib/screens/classacad.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
+
+class Acad {
+  String grade;
+  String subject;
+
+
+ Acad({
+    @required this.grade,
+    @required this.subject,
+    }
+    );
+}
