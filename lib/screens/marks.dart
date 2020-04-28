a570ee43f1149cdb99639cd3fb9841b435655f27
maa-atk <maa.abhitk@gmail.com>
2020-04-28 22:05:38 +0530

    Update=>marks

--- lib/screens/marks.dart
+++ lib/screens/marks.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'marksheet.dart';
 
 class Marks extends StatefulWidget {
   Marks(this.marks);
@@ -8,36 +9,179 @@ class Marks extends StatefulWidget {
 }
 
 class _MarksState extends State<Marks> {
-  List<Map<String, dynamic>> m;
   List<String> courses;
   List<dynamic> mark;
+
+  List<Marksheet> marele = [];
+  List<Exam> exeele = [];
+
   @override
   void initState() {
     super.initState();
     getData();
   }
 
+  // void getData() {
+  //   m = [];
+  //   courses = widget.marks["Marks"].keys.toList();
+  //   mark = widget.marks["Marks"].values.toList();
+  //   for (var i = 0; i < widget.marks["Marks"].length; i++) {
+  //     m.add(widget.marks["Marks"][i]);
+  //   }
+  //   print(widget.marks);
+  //   //print(courses);
+  //   //print(mark);
+  //   //print(m);
+  // }
+
   void getData() {
-    m = [];
     courses = widget.marks["Marks"].keys.toList();
     mark = widget.marks["Marks"].values.toList();
-//    for (var i = 0; i < widget.marks["Marks"].length; i++) {
-//      m.add(widget.marks["Marks"][i]);
-//    }
-//    print(courses);
-//    print(mark);
-    print(m);
+    // print(widget.marks["Marks"].values.toList()[0]);
+    // acad.forEach((k, v) => courses.add(
+    //       k,
+    //     ));
+
+    //print(mark);
+    marele = [];
+    var num = 0;
+    while (num < courses.length) {
+      marele.add(Marksheet(subject: courses[num], group: mark[num]));
+      num++;
+    }
+  }
+
+  Widget marlist() {
+    return Container(
+        width: double.infinity,
+        child: SingleChildScrollView(
+            child: Column(
+          children: marele.map((mr) {
+            return Container(
+              width: double.infinity,
+              child: Card(
+                color: Colors.transparent,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 15),
+                      child: Text(mr.subject,
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18,
+                          )),
+                    ),
+                    Container(
+                        width: double.infinity,
+                        padding: EdgeInsets.all(5.0),
+                        decoration: BoxDecoration(
+                            gradient: LinearGradient(
+                                begin: Alignment.topRight,
+                                end: Alignment.bottomLeft,
+                                colors: [
+                                  Color.fromRGBO(229, 45, 39, 100),
+                                  Color.fromRGBO(179, 18, 23, 100),
+
+                                  // Colors.deepPurpleAccent,
+                                  // Colors.pinkAccent
+                                ]),
+                            borderRadius: BorderRadius.circular(7.0),
+                            border: Border.all(
+                              width: .003,
+                            )),
+                        child: minimar(mr.group)),
+                  ],
+                ),
+              ),
+            );
+          }).toList(),
+        )));
+  }
+
+  Widget minimar(var group) {
+    List<dynamic> examnames = [];
+    List<dynamic> examvalues = [];
+    List<Exam> exeele = [];
+    group.forEach((k, v) => examnames.add(k));
+
+    group.forEach((k, v) => examvalues.add(v));
+
+    var num = 0;
+    while (num < examnames.length) {
+      exeele.add(Exam(exname: examnames[num], val: examvalues[num]));
+      num++;
+    }
+
+    return Container(
+        color: Colors.black,
+        padding: EdgeInsets.all(10),
+        child: SingleChildScrollView(
+            child: Column(
+          children: exeele.map((e) {
+            return Container(
+              child: Column(
+                children: <Widget>[
+                  Card(
+                    color: Colors.transparent,
+                    elevation: 0,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Text(e.exname,
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16,
+                            )),
+                        Container(
+                          width: 50,
+                          height: 50,
+                          color: Color.fromRGBO(450, 0, 0, 5),
+                          child: Container(
+                            child: Card(
+                              color: Colors.black,
+                              child: Center(
+                                child: Text(e.val,
+                                    style: TextStyle(
+                                      fontSize: 18,
+                                    )),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Divider(color: Colors.grey)
+                ],
+              ),
+            );
+          }).toList(),
+        )));
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: ListView.builder(
-        itemCount: courses.length,
-        itemBuilder: (BuildContext context, int index) {
-          return Text('');
-        },
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        titleSpacing: 61,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.vertical(
+            top: Radius.circular(15),
+          ),
+        ),
+        title: Text('Marks'),
+        backgroundColor: Color.fromRGBO(200, 25, 25, 60),
+      ),
+      body: Container(
+        child: Padding(
+          padding: const EdgeInsets.all(15),
+          child: SingleChildScrollView(
+            scrollDirection: Axis.vertical,
+            child: marlist(),
+          ),
+        ),
       ),
     );
   }
--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-//import 'package:vitask/api.dart';
+import 'package:vitask/api.dart';
 import 'package:vitask/constants.dart';
 import 'package:animated_text_kit/animated_text_kit.dart';
 import 'dashboard.dart';
 import 'package:modal_progress_hud/modal_progress_hud.dart';
-//import 'package:vitask/database/StudentModel.dart';
+import 'package:vitask/database/StudentModel.dart';
 import 'package:vitask/database/Student_DAO.dart';
 
 class WelcomeScreen extends StatefulWidget {
@@ -89,53 +89,54 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
                           showSpinner = true;
                         });
                         //Run this part to get the data from all the APIs and store it in the database.
-//                        url =
-//                            'https://vitask.me/authenticate?username=$regNo&password=$password';
-//                        API api = API();
-//                        Map<String, dynamic> profileData =
-//                            await api.getAPIData(url);
-//                        String t = profileData['APItoken'].toString();
-//                        String u = profileData['RegNo'].toString();
-//                        Map<String, dynamic> attendanceData =
-//                            await api.getAPIData(
-//                                'https://vitask.me/classesapi?token=$t');
-//                        print('Classes');
-//                        Map<String, dynamic> timeTableData =
-//                            await api.getAPIData(
-//                                'https://vitask.me/timetableapi?token=$t');
-//                        print('Time Table');
-//                        Map<String, dynamic> marksData = await api
-//                            .getAPIData('https://vitask.me/marksapi?token=$t');
-//                        print('Marks');
-//                        Map<String, dynamic> acadHistoryData =
-//                            await api.getAPIData(
-//                                'https://vitask.me/acadhistoryapi?token=$t');
-//                        print('AcadHistory');
-//
-//                        Student student = Student(
-//                            profileKey: (u + "-profile"),
-//                            profile: profileData,
-//                            attendanceKey: (u + "-attendance"),
-//                            attendance: attendanceData,
-//                            timeTableKey: (u + "-timeTable"),
-//                            timeTable: timeTableData,
-//                            marksKey: (u + "-marks"),
-//                            marks: marksData,
-//                            acadHistoryKey: (u + "-acadHistory"),
-//                            acadHistory: acadHistoryData);
-//                        StudentDao().deleteStudent(student);
-//                        StudentDao().insertStudent(student);
+                        // url =
+                        //     'https://vitask.me/authenticate?username=18BLC1095&password=Durjanatoz2000';
+                        // API api = API();
+                        // Map<String, dynamic> profileData =
+                        //     await api.getAPIData(url);
+                        // String t = profileData['APItoken'].toString();
+                        // String u = profileData['RegNo'].toString();
+                        // Map<String, dynamic> attendanceData =
+                        //     await api.getAPIData(
+                        //         'https://vitask.me/classesapi?token=$t');
+                        // print('Classes');
+                        // Map<String, dynamic> timeTableData =
+                        //     await api.getAPIData(
+                        //         'https://vitask.me/timetableapi?token=$t');
+                        // print('Time Table');
+                        // Map<String, dynamic> marksData = await api
+                        //     .getAPIData('https://vitask.me/marksapi?token=$t');
+                        // print('Marks');
+                        // Map<String, dynamic> acadHistoryData =
+                        //     await api.getAPIData(
+                        //         'https://vitask.me/acadhistoryapi?token=$t');
+                        // print('AcadHistory');
+
+                        // Student student = Student(
+                        //     profileKey: (u + "-profile"),
+                        //     profile: profileData,
+                        //     attendanceKey: (u + "-attendance"),
+                        //     attendance: attendanceData,
+                        //     timeTableKey: (u + "-timeTable"),
+                        //     timeTable: timeTableData,
+                        //     marksKey: (u + "-marks"),
+                        //     marks: marksData,
+                        //     acadHistoryKey: (u + "-acadHistory"),
+                        //     acadHistory: acadHistoryData);
+                        // StudentDao().deleteStudent(student);
+                        // StudentDao().insertStudent(student);
                         //Run this part for fetching the stored data from the database. Note the key value,i.e, "18BLC1082-profile, etc."
                         Map<String, dynamic> p =
-                            (await StudentDao().getData("18BLC1082-profile"));
+                            (await StudentDao().getData("18BLC1095-profile"));
                         Map<String, dynamic> att = (await StudentDao()
-                            .getData("18BLC1082-attendance"));
+                            .getData("18BLC1095-attendance"));
                         Map<String, dynamic> tt =
-                            (await StudentDao().getData("18BLC1082-timeTable"));
+                            (await StudentDao().getData("18BLC1095-timeTable"));
                         Map<String, dynamic> m =
-                            (await StudentDao().getData("18BLC1082-marks"));
+                            (await StudentDao().getData("18BLC1095-marks"));
                         Map<String, dynamic> ah = (await StudentDao()
-                            .getData("18BLC1082-acadHistory"));
+                            .getData("18BLC1095-acadHistory"));
+                        print(att);
                         Navigator.push(
                           context,
                           MaterialPageRoute(
@@ -161,7 +162,7 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
                 ),
                 FloatingActionButton(onPressed: () async {
                   Map<String, dynamic> hi =
-                      (await StudentDao().getData("18BLC1083-marks"));
+                      (await StudentDao().getData("18BLC1095-marks"));
                   print(hi["Marks"]);
                 })
               ],
