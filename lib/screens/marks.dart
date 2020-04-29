1f7ecb8b3878e3ca6268035b9b5261c10f4ed143
maa-atk <maa.abhitk@gmail.com>
2020-04-29 11:49:19 +0530

    glow effectmar

--- lib/screens/marks.dart
+++ lib/screens/marks.dart
@@ -61,49 +61,76 @@ class _MarksState extends State<Marks> {
               width: double.infinity,
               child: Card(
                 color: Colors.transparent,
-                child: Container(
-                  padding: EdgeInsets.all(1.0),
-                  decoration: BoxDecoration(
-                    gradient: LinearGradient(
-                        begin: Alignment.topRight,
-                        end: Alignment.bottomLeft,
-                        colors: [
-                          Color.fromRGBO(229, 45, 39, 100),
-                          Color.fromRGBO(179, 18, 23, 100),
+                elevation: 0,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(20),
+                  child: Container(
+                    padding: EdgeInsets.all(3.0),
+                    // decoration: BoxDecoration(
+                    //   gradient: LinearGradient(
+                    //       begin: Alignment.topRight,
+                    //       end: Alignment.bottomLeft,
+                    //       colors: [
+                    //         // Color.fromRGBO(229, 45, 39, 100),
+                    //         // Color.fromRGBO(179, 18, 23, 100),
+                    //         Colors.blue[900],
+                    //         Colors.indigo,
+                    //       ]),
+                    // ),
+                    decoration:
+                        BoxDecoration(borderRadius: BorderRadius.circular(0),
+                            // gradient: LinearGradient(
+                            //     begin: Alignment.topRight,
+                            //     end: Alignment.bottomLeft,
+                            //     colors: [
+                            //       // Color.fromRGBO(229, 45, 39, 100),
+                            //       // Color.fromRGBO(179, 18, 23, 100),
+                            //       Colors.blue[900],
+                            //       Colors.indigo,
+                            //     ]),
+                            boxShadow: [
+                          BoxShadow(
+                            color: Colors.indigo,
+                            blurRadius: 3,
+                            spreadRadius: 0.3,
+                          ),
                         ]),
-                  ),
-                  child: Card(
-                    color: Colors.black,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Padding(
-                          padding: const EdgeInsets.symmetric(
-                              vertical: 15, horizontal: 10),
-                          child: Text(mr.subject,
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 18,
-                              )),
-                        ),
-                        Container(
-                            width: double.infinity,
-                            padding: EdgeInsets.all(10.0),
-                            // decoration: BoxDecoration(
-                            //   gradient: LinearGradient(
-                            //       begin: Alignment.topRight,
-                            //       end: Alignment.bottomLeft,
-                            //       colors: [
-                            //         Color.fromRGBO(229, 45, 39, 100),
-                            //         Color.fromRGBO(179, 18, 23, 100),
+                    child: Card(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(16.0),
+                      ),
+                      color: Colors.black,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Padding(
+                            padding: EdgeInsets.symmetric(
+                                vertical: 20, horizontal: 20),
+                            child: Text(mr.subject,
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 18,
+                                )),
+                          ),
+                          Container(
+                              width: double.infinity,
+                              padding: EdgeInsets.all(10.0),
+                              // decoration: BoxDecoration(
+                              //   gradient: LinearGradient(
+                              //       begin: Alignment.topRight,
+                              //       end: Alignment.bottomLeft,
+                              //       colors: [
+                              //         Color.fromRGBO(229, 45, 39, 100),
+                              //         Color.fromRGBO(179, 18, 23, 100),
 
-                            //         // Colors.deepPurpleAccent,
-                            //         // Colors.pinkAccent
-                            //       ]),
-                            //   borderRadius: BorderRadius.circular(7.0),
-                            // ),
-                            child: minimar(mr.group)),
-                      ],
+                              //         // Colors.deepPurpleAccent,
+                              //         // Colors.pinkAccent
+                              //       ]),
+                              //   borderRadius: BorderRadius.circular(7.0),
+                              // ),
+                              child: minimar(mr.group)),
+                        ],
+                      ),
                     ),
                   ),
                 ),
@@ -147,18 +174,23 @@ class _MarksState extends State<Marks> {
                               fontWeight: FontWeight.bold,
                               fontSize: 16,
                             )),
-                        Container(
-                          width: 50,
-                          height: 50,
-                          color: Color.fromRGBO(450, 0, 0, 5),
+                        ClipOval(
                           child: Container(
-                            child: Card(
-                              color: Colors.black,
-                              child: Center(
-                                child: Text(e.val,
-                                    style: TextStyle(
-                                      fontSize: 18,
-                                    )),
+                            width: 40,
+                            height: 40,
+                            //color: Color.fromRGBO(450, 0, 0, 5),
+                            color: Colors.indigo,
+                            child: Container(
+                              child: Card(
+                                color: Colors.transparent,
+                                elevation: 0,
+                                child: Center(
+                                  child: Text(e.val,
+                                      style: TextStyle(
+                                        color: Colors.greenAccent[100],
+                                        fontSize: 18,
+                                      )),
+                                ),
                               ),
                             ),
                           ),
@@ -176,26 +208,46 @@ class _MarksState extends State<Marks> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return new Scaffold(
       backgroundColor: Colors.black,
-      appBar: AppBar(
-        titleSpacing: 61,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.vertical(
-            top: Radius.circular(15),
+      // appBar: AppBar(
+      //   titleSpacing: 61,
+      //   shape: RoundedRectangleBorder(
+      //     borderRadius: BorderRadius.vertical(
+      //         bottom: Radius.circular(20), top: Radius.circular(20)),
+      //   ),
+      //   title: Text('Marks'),
+      //   backgroundColor: Color.fromRGBO(200, 25, 25, 60),
+      // ),
+      body: CustomScrollView(
+        slivers: <Widget>[
+          SliverAppBar(
+            expandedHeight: 90,
+            floating: true,
+            pinned: false,
+            flexibleSpace: FlexibleSpaceBar(
+              title: Text(
+                "Marks",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                ),
+              ),
+              background: Image.asset("images/side.jpg", fit: BoxFit.cover),
+            ),
           ),
-        ),
-        title: Text('Marks'),
-        backgroundColor: Color.fromRGBO(200, 25, 25, 60),
-      ),
-      body: Container(
-        child: Padding(
-          padding: const EdgeInsets.all(15),
-          child: SingleChildScrollView(
-            scrollDirection: Axis.vertical,
-            child: marlist(),
+          SliverFillRemaining(
+            child: Container(
+              child: Padding(
+                padding: const EdgeInsets.all(15),
+                child: SingleChildScrollView(
+                  scrollDirection: Axis.vertical,
+                  child: marlist(),
+                ),
+              ),
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
